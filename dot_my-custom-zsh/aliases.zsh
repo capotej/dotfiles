d2b2758530cff8cab28bcd33ff8d8a231e60f7c2
Julio Capote <jcapote@gmail.com>
2023-02-24 21:34:11 -0500

    use zsh git plugin and add kubecontext to p10k

--- dot_my-custom-zsh/aliases.zsh
+++ dot_my-custom-zsh/aliases.zsh
@@ -1,7 +1,3 @@
-function mkcd() {
-	mkdir -p $1
-	cd $1
-}
 alias bc='bc -l'
 alias xmlpp='xmllint --format -'
 alias jsonpp='python -m json.tool'
@@ -9,60 +5,18 @@ alias cm='chezmoi'
 alias aliasedit='chezmoi edit '
 alias vim='nvim'
 alias prweb='gh pr view --web'
+alias serve="myip; python -m SimpleHTTPServer"
+alias myip="ifconfig | egrep -e '[0-9][0-9][0-9]\.' | grep -v '127.0.0.1'"
 
 [[ "$TERM" == "xterm-kitty" ]] && alias ssh="kitty +kitten ssh"
 
 function bdd() {
-  git for-each-ref --sort=-committerdate refs/heads/ --format='%(committerdate) %(refname:short)' | tac | tail
+  git for-each-ref --sort=-committerdate refs/heads/ --format='%(committerdate) %(refname:short)' | tail -r
 }
-
-function raw_git_branch() {
-  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
-}
-
-alias serve="myip; python -m SimpleHTTPServer"
-alias myip="ifconfig | egrep -e '[0-9][0-9][0-9]\.' | grep -v '127.0.0.1'"
-
-# git
-alias checkout="git checkout "
-alias fetch="git fetch "
-alias co="git checkout "
-alias dcommit="git svn dcommit "
-alias gpos="git push origin staging"
-alias gpod="git push origin dev"
-alias gde="git diff | $EDITOR"
-alias ga="git commit --amend"
-alias gcs="git checkout staging"
-alias gcm="git checkout master"
-alias gc="git commit -am "
-alias commit="git commit"
-alias pull="git pull"
-alias push="git push"
-alias pom="git push origin master"
-alias po="git pull origin "
-alias psh="git push origin "
-alias pp="pull origin master && push origin master"
-
 function pushit() {
-  git push origin $(raw_git_branch)
+  git push origin $(current_branch)
 }
-
 function pullit() {
-  git pull origin $(raw_git_branch)
+  git pull origin $(current_branch)
 }
 
-alias rebase="git rebase"
-alias add="git add "
-alias branch="git branch "
-alias stash="git stash "
-alias status="git status "
-alias s="git status "
-alias init="git init "
-alias merge="git merge "
-alias mt="git mergetool "
-alias fuckit="git reset --hard"
-alias back="co -"
-
-function gca() {
-	git commit -a -m "$*"
-}
--- dot_my-custom-zsh/vars.zsh
+++ dot_my-custom-zsh/vars.zsh
@@ -1,1 +1,1 @@
-export EDITOR='code -w'
+export EDITOR='code --wait --new-window'
